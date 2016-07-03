bec5f37b1cc6660dc2a6433d206871eea2387a4c
tongx <ly012599@qq.com>
2016-07-03 09:26:01 +0800

    teacher sidebar

--- app/js/controllers/educate/stuInfo.js
+++ app/js/controllers/educate/stuInfo.js
@@ -1,6 +1,14 @@
 'use strict';
 angular.module('homeApp.educate')
 	.controller('teaStuInfo', function($scope, $routeParams, fetchStuInfoById) {
+		$scope.sidebar = [{
+			"name": '基本信息',
+			"link": '#basic',
+			"default": 1
+		},{
+			"name": '家长信息',
+			"link": '#parent'
+		}]
 		fetchStuInfoById($routeParams, function(result) {
 			console.log(result);
 			$scope.$apply(function() {
