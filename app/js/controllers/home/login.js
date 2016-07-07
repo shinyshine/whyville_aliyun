b89f12133c58b116f1684415087b9f2a148534b4
tongx <ly012599@qq.com>
2016-07-07 19:18:08 +0800

    reset pwd md5

--- app/js/controllers/home/login.js
+++ app/js/controllers/home/login.js
@@ -63,7 +63,11 @@ angular.module('homeApp.home')
 					alert('两次密码不一致');
 					return false;
 				}
-				modifyPwd($scope.postData, function(result) {
+				var data = {
+					new_pwd: $.md5($scope.postData.new_pwd),
+					re_pwd: $.md5($scope.postData.re_pwd)
+				}
+				modifyPwd(data, function(result) {
 					if(result.status == 1) {
 						alert('成功修改密码');
 						$scope.$apply(function() {
