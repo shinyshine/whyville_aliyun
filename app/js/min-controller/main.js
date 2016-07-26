41f421e6fb878f97309e47ce6bb89f6e3261c906
tongx <ly012599@qq.com>
2016-07-26 10:08:38 +0800

    stuattend

--- app/js/min-controller/main.js
+++ app/js/min-controller/main.js
@@ -51,7 +51,10 @@ var app = angular.module('homeApp', ['ngRoute', 'ngCookies', 'tm.pagination', 'p
         if(result.status == 1) {
           //$cookies.remove('user_id');
           window.location.href = ROOT + 'login';
-          window.location.reload();
+          setTimeout(function() {
+            window.location.reload();
+          }, 400)
+          
         }
         
       })
@@ -3804,6 +3807,83 @@ angular.module('homeApp.educate', ['ngRoute', 'homeApp.educateService'])
 				controller: 'teaReport'
 			})
 	})
+
+'use strict';
+angular.module('homeApp.educate')
+  .controller('stuAttendList', function($scope, $routeParams, fetchStuAttTable) {
+    fetchStuAttTable($routeParams, function(result) {
+      $scope.courseAttend = result;
+      $scope.$apply();
+    })
+  })
+  
+  //教师修改学生的出勤状态
+  .controller('teaStuAttend', function($scope, $routeParams, allAttend, fetchStuAttend, modifyStuAttend) {
+    console.log($routeParams);
+    $scope.attendType = [{
+      "id": '0',
+      "name": '缺勤'
+    },{
+      "id": '1',
+      "name": '已到'
+    }]
+
+    
+    fetchStuAttend($routeParams, function(result) {
+      console.log(result);
+      $scope.courseStuInfo = result;
+      $scope.$apply();
+    })
+
+    $scope.modify = function(index) {
+      var item = $scope.courseStuInfo.stuInfo[index];
+      if(item.isEditing) {
+        var postData = {
+          "course_id": $routeParams.course_id,
+          "stu_id": item.stu_id,
+          "stu_state": item.stu_state
+        }
+        modifyStuAttend(postData, function(result) {
+          callbackAlert(result.status, '修改成功');
+        })
+        
+      }
+      $scope.courseStuInfo.stuInfo[index].isEditing = !item.isEditing;
+    }
+
+    $scope.allAttend = function() {
+      allAttend($routeParams, function(result) {
+        if(result.status == 1) {
+          alert('操作成功');
+          window.location.reload();
+        }
+      })
+    }
+  })
+  
+  .controller('homework', function($scope, $routeParams, fetchHomeworks,modHomework) {
+    fetchHomeworks($routeParams, function(result) {
+      $scope.courseStuInfo = result;
+      $scope.$apply();
+    })
+
+    $scope.modify = function(index) {
+      var item = $scope.courseStuInfo.stuInfo[index],
+        status = item.isEditing;
+      if(status) {
+        var postData = {
+          "stu_id": item.stu_id,
+          "att_id": item.course_id,
+          "comment": item.homework_state
+        }
+        console.log(postData);
+        modHomework(postData, function(result) {
+          callbackAlert(result.status, '修改成功');
+        }) 
+      }
+      $scope.courseStuInfo.stuInfo[index].isEditing = !status;  
+    }
+  })
 'use strict';
 angular.module('homeApp.educateService', [])
 	.factory('eduAPI', function(server) {
